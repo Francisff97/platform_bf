209834818648a675cef6f5ba038e5510a11cb572
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-07 02:08:39 +0200

    Create admin.blade.php

---
+++ resources/views/emails/contact/admin.blade.php
@@ -0,0 +1,12 @@
+{{-- resources/views/emails/contact/admin.blade.php --}}
+@component('mail::message')
+# New contact request
+
+**From:** {{ $data['name'] }} ({{ $data['email'] }})  
+**Subject:** {{ $data['subject'] ?? '—' }}
+
+@component('mail::panel')
+{{ $data['message'] }}
+@endcomponent
+
+@endcomponent
