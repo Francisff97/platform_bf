d0c211d656d114e50c27ed70027ca916da8181d8
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-09 00:42:49 +0200

    Update img.blade.php

--- resources/views/components/img.blade.php
+++ resources/views/components/img.blade.php
@@ -1,22 +1,34 @@
-<?php
 @props([
   'src',
-  'alt' => null,
-  'class' => '',
-  'width' => null,
-  'height' => null,
-  'loading' => null,
+  'alt'     => null,
+  'class'   => '',
+  'width'   => null,
+  'height'  => null,
+  'loading' => null,   // true/false oppure 'lazy'/'eager' (se vuoi forzare)
 ])
 
 @php
+  // Prendi i valori dal catalogo SEO per questa URL (alt, lazy di default)
   $attrs = \App\Support\SeoManager::imgAttrsByUrl($src);
-  $altFinal = $alt ?? ($attrs['alt'] ?? '');
-  $lazy = $loading ?? (($attrs['lazy'] ?? true) ? 'lazy' : null);
+
+  // ALT: se passo un alt esplicito lo uso, altrimenti quello del catalogo, altrimenti stringa vuota
+  $altFinal = (isset($alt) && $alt !== null && $alt !== '')
+    ? $alt
+    : (isset($attrs['alt']) ? $attrs['alt'] : '');
+
+  // LAZY: se passo loading esplicito lo rispetto, altrimenti fallback al catalogo (default: true)
+  if ($loading === 'lazy' || $loading === 'eager') {
+    $lazyFlag = ($loading === 'lazy');
+  } elseif ($loading === true || $loading === false) {
+    $lazyFlag = (bool) $loading;
+  } else {
+    $lazyFlag = isset($attrs['lazy']) ? (bool)$attrs['lazy'] : true;
+  }
 @endphp
 
 <img src="{{ $src }}"
-     @if(!is_null($altFinal)) alt="{{ $altFinal }}" @endif
-     @if($width) width="{{ $width }}" @endif
-     @if($height) height="{{ $height }}" @endif
-     @if($lazy) loading="{{ $lazy }}" @endif
+     @if($altFinal !== '') alt="{{ $altFinal }}" @endif
+     @if(!empty($width))  width="{{ $width }}"   @endif
+     @if(!empty($height)) height="{{ $height }}"  @endif
+     loading="{{ $lazyFlag ? 'lazy' : 'eager' }}"
      class="{{ $class }}">
