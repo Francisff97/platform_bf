a2a24a9c3ba1539e706d5eece3b33c9677d02777
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-08 14:38:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -116,7 +116,7 @@ Route::middleware(['auth', AdminOnly::class])
     ->group(function () {
         // Add-on Discord (queste esistevano già: lasciate invariate)
         Route::prefix('addons')->name('addons.')->group(function () {
-            Route::get('discord', [\App\Http\Controllers\Admin\DiscordAddonsController::class,'index'])
+            Route::get('discord', [\App\Http\Controllers\Admin\DiscordAddonsController::class,'edit'])
                 ->name('discord');
             Route::post('discord', [\App\Http\Controllers\Admin\DiscordAddonsController::class,'save'])
                 ->name('discord.save');
