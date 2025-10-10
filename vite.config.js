ec383da82c359081494157c14a929787081f72c3
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-10 17:37:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-      buildDirectory: 'build',
-    }),
-  ],
-  build: {
-    outDir: 'public/build',
-    assetsDir: '',          // niente sottocartella
-    manifest: false,        // ⬅️ disattiva manifest
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        // nomi deterministici, senza hash
-        entryFileNames: 'app.js',
-        chunkFileNames: '[name].js',
-        assetFileNames: (assetInfo) => {
-          // assicura "app.css" per l’estrazione CSS
-          if (assetInfo.name && assetInfo.name.endsWith('.css')) return 'app.css'
-          return '[name][extname]'
-        },
-      },
-    },
-  },
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
