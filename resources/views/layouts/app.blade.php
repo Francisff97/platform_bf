2e73ba3cdab62c952e7e84075027cb5ed16ec66b
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-10 15:26:20 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,10 @@
       </script>
       <!-- End Google Tag Manager -->
     @endif
-
+   {{-- Google Fonts: Orbitron --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700&display=swap" rel="stylesheet">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
