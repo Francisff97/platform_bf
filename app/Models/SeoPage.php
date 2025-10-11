e37eef261a9fe7a3c962bafd2fe2258e611f2710
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-11 11:17:03 +0200

    Update SeoPage.php

--- app/Models/SeoPage.php
+++ app/Models/SeoPage.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class SeoPage extends Model
 {
-    protected $fillable = ['route_name','path','meta_title','meta_description','og_image_path'];
+    protected $fillable = [
+        'route_name','path',
+        'meta_title','meta_description','og_image_path',
+    ];
 }
