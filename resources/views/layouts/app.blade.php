2be0dc028efe6c405022c1f1600c4df5ea4b76a7
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-13 11:07:01 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
   {{-- === Performance hints (keep at most ~4 preconnects) === --}}
   <link rel="dns-prefetch" href="//fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.googleapis.com">
