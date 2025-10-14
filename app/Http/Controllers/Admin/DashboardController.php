aff908119f9b3272e11f5428737e8e81605b5565
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-14 12:39:25 +0200

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\User;
+use App\Models\Coupon;
 
 class DashboardController extends Controller
 {
