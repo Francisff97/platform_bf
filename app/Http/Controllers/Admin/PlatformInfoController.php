ea08f65d9dbccf528525de0c6c3872faf2705abb
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-14 09:14:58 +0200

    Update PlatformInfoController.php

--- app/Http/Controllers/Admin/PlatformInfoController.php
+++ app/Http/Controllers/Admin/PlatformInfoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class PlatformInfoController extends Controller
 {
