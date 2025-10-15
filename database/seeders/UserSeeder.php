e5a1879b25d4123cdaf5219de3f9cbb0267eeba7
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-15 19:43:47 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Hash;
 class DemoUserSeeder extends Seeder {
     public function run() {
         $u = User::firstOrCreate(
-            ['email' => 'admin-demo@base-forge.com'],
+            ['email' => 'admindemo@base-forge.com'],
             [
                'name' => 'Admin Demo',
-               'password' => Hash::make('demo-password-123'), // cambialo se vuoi
+               'password' => Hash::make('admindemo-123'), // cambialo se vuoi
                'role' => 'admin_view',
                'is_demo' => true,
             ]
