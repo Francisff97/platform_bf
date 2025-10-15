1a44588d30d48176c8d7a25f512dfc75d7ea6683
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-15 19:43:19 +0200

    Update 2025_10_15_add_fake_adimn_to_users.php

--- database/migrations/2025_10_15_add_fake_adimn_to_users.php
+++ database/migrations/2025_10_15_add_fake_adimn_to_users.php
@@ -1,14 +1,23 @@
 <?php
-public function up()
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::table('users', function (Blueprint $table) {
-        $table->string('role')->default('user')->after('email');
-        $table->boolean('is_demo')->default(false)->after('role');
-    });
-}
-public function down()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn(['role','is_demo']);
-    });
-}
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user');
+            $table->boolean('is_demo')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'is_demo']);
+        });
+    }
+};
