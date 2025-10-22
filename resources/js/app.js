861c0d3cf2314adfb65fa61cd059fb91b54ce2cc
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-10-22 12:35:19 +0200

    Remove service worker registration logic
    Removed service worker registration code from app.js.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {});
-  });
-}
