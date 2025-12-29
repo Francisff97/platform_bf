e85179d5041f5d6b3ec98ccdd4b08a7f4c063810
Francisff97 <67428751+Francisff97@users.noreply.github.com>
2025-12-29 17:29:28 +0100

    Simplify Vite bundle inclusion
    Removed conditional check for Vite manifest file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -183,9 +183,7 @@
   <!-- ===============================
        🧩 Vite bundle
   =============================== -->
-  @if (file_exists(public_path('build/manifest.json')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-@endif
 
 </head>
 
